065b007530b412275e05c368c94b22847fcad759
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2024-02-13 00:15:46 +0530

    Improved readability

--- lib/app/modules/verify/controllers/verify_controller.dart
+++ lib/app/modules/verify/controllers/verify_controller.dart
@@ -1,3 +1,4 @@
+
 // ignore_for_file: avoid_print
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,42 +7,40 @@ import 'package:get/get.dart';
 import 'package:itsurgent/app/routes/app_pages.dart';
 
 class VerifyController extends GetxController {
-  RxString pin = "".obs;
+  RxString pin = ''.obs;
+
   void verifyOtp() async {
     try {
-      if (Get.arguments != null && Get.arguments['verificationId'] != null) {
-        final String verificationId = Get.arguments['verificationId'];
-        final String name = Get.arguments['name'];
-
-        PhoneAuthCredential credential = PhoneAuthProvider.credential(
-          verificationId: verificationId,
-          smsCode: pin.value,
-        );
-        UserCredential userCredential =
-            await FirebaseAuth.instance.signInWithCredential(credential);
-        await saveUserData(userCredential.user!.uid, name);
-        Get.toNamed(Routes.HOME);
-      } else {
+      final arguments = Get.arguments;
+      if (arguments == null || arguments['verificationId'] == null) {
         print('Verification ID not found');
+        return;
       }
+      final String verificationId = arguments['verificationId'];
+      final String name = arguments['name'];
+      final PhoneAuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: verificationId,
+        smsCode: pin.value,
+      );
+      final UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      await saveUserData(userCredential.user!.uid, name);
+      Get.toNamed(Routes.HOME);
+      print('User logged in successfully: $name');
     } catch (e) {
       print('Error during OTP verification and sign-in: $e');
     }
   }
 
-  final count = 0.obs;
-
-  void increment() => count.value++;
-
   Future<void> saveUserData(String userId, String name) async {
     try {
-        await FirebaseFirestore.instance.collection('users').doc(userId).set({
-            'name': name,
-        });
-        // print("User data saved successfully" + name + userId);
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .set({'name': name});
+      print('User data saved successfully: $name, userId: $userId');
     } catch (e) {
-        print('Error saving user data: $e');
+      print('Error saving user data: $e');
     }
-}
-
+  }
 }
