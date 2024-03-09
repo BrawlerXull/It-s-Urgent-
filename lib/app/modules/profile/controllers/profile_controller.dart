e100066b483a13bfbe02d134108ec3fe69a37f00
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-03-10 01:31:53 +0530

    Re implemented profile page logic

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,10 +1,11 @@
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:itsurgent/app/services/services.dart';
 
 class ProfileController extends GetxController {
-  final UserService _userService = UserService();
+  late final FirestoreService firestoreService;
+  ProfileController() {
+    firestoreService = FirestoreService();
+  }
 
   final name = ''.obs;
   final email = ''.obs;
@@ -17,7 +18,7 @@ class ProfileController extends GetxController {
 
   Future<void> fetchUserData() async {
     try {
-      final userData = await _userService.getUserData();
+      final userData = await firestoreService.getUserData();
       name.value = userData['name'] ?? 'Please Update the Name';
       email.value = userData['email'] ?? 'Please Update the Email';
     } catch (e) {
@@ -27,41 +28,11 @@ class ProfileController extends GetxController {
 
   Future<void> updateUserData(String newName, String newEmail) async {
     try {
-      await _userService.updateUserData(newName, newEmail);
+      await firestoreService.updateUserData(newName, newEmail);
       name.value = newName;
       email.value = newEmail;
     } catch (e) {
       print('Error updating user data: $e');
     }
-  }
-}
-
-
-class UserService {
-  final _firestore = FirebaseFirestore.instance;
-  final _auth = FirebaseAuth.instance;
-
-  Future<Map<String, dynamic>> getUserData() async {
-    final userId = _auth.currentUser?.uid;
-    if (userId == null) {
-      throw Exception('User is not logged in.');
-    }
-    final userSnapshot = await _firestore.collection('users').doc(userId).get();
-    if (userSnapshot.exists) {
-      return userSnapshot.data() as Map<String, dynamic>;
-    } else {
-      throw Exception('User document does not exist.');
-    }
-  }
-
-  Future<void> updateUserData(String newName, String newEmail) async {
-    final userId = _auth.currentUser?.uid;
-    if (userId == null) {
-      throw Exception('User is not logged in.');
-    }
-    await _firestore.collection('users').doc(userId).update({
-      'name': newName,
-      'email': newEmail,
-    });
   }
 }
--- lib/app/services/firestore_service.dart
+++ lib/app/services/firestore_service.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirestoreService {
-
   final _auth = FirebaseAuth.instance;
   final _firestore = FirebaseFirestore.instance;
 
@@ -27,6 +26,30 @@ class FirestoreService {
       'fcmToken': fcmToken,
     }).then((value) {
       print("FCM Updated ${fcmToken}");
+    });
+  }
+
+  Future<Map<String, dynamic>> getUserData() async {
+    final userId = _auth.currentUser?.uid;
+    if (userId == null) {
+      throw Exception('User is not logged in.');
+    }
+    final userSnapshot = await _firestore.collection('users').doc(userId).get();
+    if (userSnapshot.exists) {
+      return userSnapshot.data() as Map<String, dynamic>;
+    } else {
+      throw Exception('User document does not exist.');
+    }
+  }
+
+  Future<void> updateUserData(String newName, String newEmail) async {
+    final userId = _auth.currentUser?.uid;
+    if (userId == null) {
+      throw Exception('User is not logged in.');
+    }
+    await _firestore.collection('users').doc(userId).update({
+      'name': newName,
+      'email': newEmail,
     });
   }
 }
