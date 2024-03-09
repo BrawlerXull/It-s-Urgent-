dd05ded13d35006c9e593b37d88aa3257d6d37d2
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-03-10 01:35:01 +0530

    Updated firestore service

--- lib/app/services/firestore_service.dart
+++ lib/app/services/firestore_service.dart
@@ -1,19 +1,18 @@
+// ignore_for_file: avoid_print
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirestoreService {
-  final _auth = FirebaseAuth.instance;
-  final _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<void> saveUserData(String userId, String name) async {
     try {
-      await FirebaseFirestore.instance
-          .collection('users')
-          .doc(userId)
-          .set({'name': name});
-      print('User data saved successfully: $name, userId: $userId');
+      await _firestore.collection('users').doc(userId).set({'name': name});
     } catch (e) {
       print('Error saving user data: $e');
+      rethrow; 
     }
   }
 
@@ -22,11 +21,15 @@ class FirestoreService {
     if (userId == null) {
       throw Exception('User is not logged in.');
     }
-    await _firestore.collection('users').doc(userId).update({
-      'fcmToken': fcmToken,
-    }).then((value) {
-      print("FCM Updated ${fcmToken}");
-    });
+    try {
+      await _firestore.collection('users').doc(userId).update({
+        'fcmToken': fcmToken,
+      });
+      print("FCM Updated $fcmToken");
+    } catch (e) {
+      print('Error updating FCM token: $e');
+      rethrow;
+    }
   }
 
   Future<Map<String, dynamic>> getUserData() async {
@@ -34,11 +37,17 @@ class FirestoreService {
     if (userId == null) {
       throw Exception('User is not logged in.');
     }
-    final userSnapshot = await _firestore.collection('users').doc(userId).get();
-    if (userSnapshot.exists) {
-      return userSnapshot.data() as Map<String, dynamic>;
-    } else {
-      throw Exception('User document does not exist.');
+    try {
+      final userSnapshot =
+          await _firestore.collection('users').doc(userId).get();
+      if (userSnapshot.exists) {
+        return userSnapshot.data() as Map<String, dynamic>;
+      } else {
+        throw Exception('User document does not exist.');
+      }
+    } catch (e) {
+      print('Error getting user data: $e');
+      rethrow;
     }
   }
 
@@ -47,9 +56,14 @@ class FirestoreService {
     if (userId == null) {
       throw Exception('User is not logged in.');
     }
-    await _firestore.collection('users').doc(userId).update({
-      'name': newName,
-      'email': newEmail,
-    });
+    try {
+      await _firestore.collection('users').doc(userId).update({
+        'name': newName,
+        'email': newEmail,
+      });
+    } catch (e) {
+      print('Error updating user data: $e');
+      rethrow; 
+    }
   }
 }
