b09a43735bc11476417a92d6a7a64ccadef69a5a
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-03-23 13:59:10 +0530

    UI added for Desktop SignUp page

--- lib/app/modules/desktopSignup/views/desktop_signup_view.dart
+++ lib/app/modules/desktopSignup/views/desktop_signup_view.dart
@@ -1,22 +1,74 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:pinput/pinput.dart';
 
 import '../controllers/desktop_signup_controller.dart';
 
 class DesktopSignupView extends GetView<DesktopSignupController> {
-  const DesktopSignupView({Key? key}) : super(key: key);
+  const DesktopSignupView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DesktopSignupView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'DesktopSignupView is working',
-          style: TextStyle(fontSize: 20),
+      extendBodyBehindAppBar: true,
+      body: Container(
+        margin: const EdgeInsets.only(left: 25, right: 25),
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 25,
+              ),
+              const Text(
+                "Enter the secret code!",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "We need to register your desktop before getting started!",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Pinput(
+                length: 6,
+                showCursor: true,
+                onChanged: (value) {
+                  // controller.pin.value = value;
+                },
+                onCompleted: (pin) {
+                  print("done");
+                  // controller.pin.value = pin;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: Get.width * 0.3,
+                height: 45,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green.shade600,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+                    onPressed: () {
+                      // controller.verifyOtp();
+                    },
+                    child: const Text(
+                      "Verify Secret Code",
+                      style: TextStyle(color: Colors.white),
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
